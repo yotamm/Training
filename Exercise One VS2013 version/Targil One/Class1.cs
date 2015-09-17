ad412e25186381b1ea6897620f6d92839ba27ece
sayag11 <sayag11@gmail.com>
2015-09-17 20:14:36 +0300

    VS2013 version

---
+++ Exercise One VS2013 version/Targil One/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil_One
+{
+    public class NoLogDefinedException : Exception
+    {
+        public NoLogDefinedException(string fileName)
+            : base(string.Format("The log file: {0} is not defined", fileName))
+        {
+            
+        }
+    }
+}
